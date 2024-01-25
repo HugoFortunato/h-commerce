803f8f8adf7eb61294e605b44e5d3d976cd930e0
Hugo Fortunato <hugo.fortunato@mobato.dev.br>
2024-01-24 22:54:09 -0300

    styling product component

---
+++ src/app/components/ProductImage.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+import { ProductType } from "../types/ProductType";
+import { useState } from "react";
+
+type ProductImageProps = {
+  product: ProductType;
+  fill?: boolean;
+};
+
+export default function ProductImage({ product, fill }: ProductImageProps) {
+  const [loading, setLoading] = useState(true);
+
+  return fill ? (
+    <Image
+      src={product.image}
+      alt={product.title}
+      fill
+      className={`object-cover ${
+        loading
+          ? "scale-110 blur-3xl grayscale"
+          : "scale-100 blur-0 grayscale-0"
+      }`}
+      onLoadingComplete={() => setLoading(false)}
+    />
+  ) : (
+    <Image
+      src={product.image}
+      alt={product.title}
+      width={400}
+      height={700}
+      className={`object-cover ${
+        loading
+          ? "scale-110 blur-3xl grayscale"
+          : "scale-100 blur-0 grayscale-0"
+      }`}
+      onLoadingComplete={() => setLoading(false)}
+    />
+  );
+}
