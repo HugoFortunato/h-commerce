9bde5c5e9dee0c4f0f00802b8791c1ffb692d55f
Hugo  Fortunato <hugo.fortunato@mobato.dev.br>
2024-02-12 11:59:03 -0300

    conectando no vercel postgress

--- src/app/components/Checkout.tsx
+++ src/app/components/Checkout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useCartStore } from '@/store';
 import { useEffect } from 'react';
 
@@ -5,17 +7,22 @@ export default function Checkout() {
   const cartStore = useCartStore();
 
   useEffect(() => {
-    fetch('/api/create-payment-intent'),
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          items: cartStore.cart,
-          payment_intent_id: cartStore.paymentIntent,
-        }),
-      };
+    fetch('/api/create-payment-intent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        items: cartStore.cart,
+        payment_intent_id: cartStore.paymentIntent,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data.paymentIntent);
+      });
   }, [cartStore.cart, cartStore.paymentIntent]);
 
   return (
